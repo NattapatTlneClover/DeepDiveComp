06d75d0ab0f770647b87bed38ff25b6702f618ed
Nattapat Sukcharoenpohn <Tlneclover@gmail.com>
2024-10-19 00:03:18 +0700

    commit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ControlComponent } from '../../../shared/control/control.component';
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ControlComponent,ButtonComponent],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
-
-}
+export class NewTicketComponent {}
